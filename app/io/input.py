46de2857e49ddaec8179d3faa47e144b29937f52
Anton Ruban <antonruban03012005@gmail.com>
2024-03-17 20:35:55 +0200

    created empty io functions

---
+++ app/io/input.py
@@ -0,0 +1,38 @@
+def input_text():
+    """Requests to input text from console.
+
+
+    Returns:
+        str: Entered text from user.
+    """
+    pass
+
+
+def read_from_file_python(file_path):
+    """Read data from a file.
+
+    Args:
+        file_path (str): Path to file from which data will be read.
+
+    Returns:
+        str: Text from read file.
+
+    Raises:
+        FileNotFoundError: If the specified file_path does not exist.
+       """
+    pass
+
+
+def read_from_file_pandas(file_path):
+    """Read data from a file using pandas.
+
+    Args:
+        file_path (str): path to file from which data will be read.
+
+    Returns:
+        pandas.DataFrame: Pandas DataFrame from read file.
+
+    Raises:
+        FileNotFoundError: If the specified file_path does not exist.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,42 @@
+def output_text(text):
+    """Prints text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+
+    Returns:
+        None
+    """
+    pass
+
+
+def write_to_file_python(file_path, content):
+    """Writes content to a Python file.
+
+    Args:
+        file_path (str): The path to the Python file to write.
+        content (str): The content to write to the file.
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If an error occurs while writing to the file.
+    """
+    pass
+
+
+def write_to_file_pandas(dataframe, file_path):
+    """
+    Writes a pandas DataFrame to a file.
+
+    Args:
+        dataframe (pandas.DataFrame): The DataFrame to write to the file.
+        file_path (str): The path to the file where the DataFrame will be written.
+
+    Returns:
+        None
+
+    Raises:
+        IOError: If an error occurs while writing to the file.
+    """
